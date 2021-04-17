1e86e54b3dc1ffc39b70d5233f72aa3eb4b93baf
Tag-Epic <tagepicuwu@gmail.com>
2021-04-17 19:12:49 +0200

    Feat: Add register endpoint

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"github.com/alexedwards/argon2id"
 	"github.com/bluemediaapp/models"
+	"github.com/bwmarrin/snowflake"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"os"
+	"time"
 )
 
 var (
@@ -27,13 +29,16 @@ func main() {
 		port:     os.Getenv("port"),
 		mongoUri: os.Getenv("mongo_uri"),
 	}
+	snowflake.Epoch = time.Date(2020, time.January, 0, 0, 0, 0, 0, time.UTC).Unix()
+	log.Print(snowflake.Epoch)
+	snowNode, _ := snowflake.NewNode(0)
 
 	app.Post("/login", func(ctx *fiber.Ctx) error {
 		userName := ctx.Get("username", "")
 		password := ctx.Get("password", "")
 
 		if userName == "" || password == "" {
-			return ctx.Status(403).SendString("username or password not provided.")
+			return ctx.Status(400).SendString("username or password not provided.")
 		}
 
 		login, err := getUserLogin(userName)
@@ -50,6 +55,48 @@ func main() {
 			return ctx.Status(403).SendString("Invalid password")
 		}
 		token, err := CreateJWTToken(login.Id)
+		if err != nil {
+			return err
+		}
+		return ctx.SendString(token)
+	})
+	app.Post("/register", func(ctx *fiber.Ctx) error {
+		userName := ctx.Get("username", "")
+		password := ctx.Get("password", "")
+		userId := snowNode.Generate().Int64()
+
+		if userName == "" || password == "" {
+			return ctx.Status(400).SendString("username or password not provided.")
+		}
+
+		_, err := getUserLogin(userName)
+		if err == nil {
+			// User already exists
+			return ctx.Status(400).SendString("Username is already taken")
+		}
+
+		hashedPassword, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+
+		userLogin := models.UserLogin{
+			Id:       userId,
+			Username: userName,
+			Password: hashedPassword,
+		}
+		user := models.DatabaseUser{
+			Id:        userId,
+			Username:  userName,
+			Interests: make(map[string]int64),
+		}
+		_, err = userLoginCollection.InsertOne(mctx, userLogin)
+		if err != nil {
+			return err
+		}
+		_, err = usersCollection.InsertOne(mctx, user)
+		if err != nil {
+			return err
+		}
+
+		token, err := CreateJWTToken(userId)
 		if err != nil {
 			return err
 		}
